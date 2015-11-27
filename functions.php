6807b65daa8866279047cd99ca6e80b25e7fb148
Lukas Albrecht <mail@lukasalbrecht.com>
2015-11-27 12:52:36 +0100

    Add first project files

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'avada-parent-stylesheet', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+function avada_lang_setup() {
+	$lang = get_stylesheet_directory() . '/languages';
+	load_child_theme_textdomain( 'Avada', $lang );
+}
+add_action( 'after_setup_theme', 'avada_lang_setup' );
+
+/*
+function register_my_menu() {
+  register_nav_menu('footer-gc-menu',__( 'Geocom Footer Menu' ));
+}
+add_action( 'init', 'register_my_menu' );
+*/
+
+
